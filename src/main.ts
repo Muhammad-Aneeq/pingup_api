7aed87ea952e9c5d198358309aff89c210585804
Aneeq Khatri <aneeqabdulsamad5761@gmail.com>
2024-06-28 10:53:13 +0500

    code updated again

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS
-  app.enableCors({
-    origin: '*', // Replace with your client's origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
