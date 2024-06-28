c6a3ede1a0595bc0afc648e9836cf84099839eaa
Aneeq Khatri <aneeqabdulsamad5761@gmail.com>
2024-06-28 10:33:29 +0500

    code updated

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS
+  app.enableCors({
+    origin: '*', // Replace with your client's origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
   await app.listen(3000);
 }
 bootstrap();
