58d66f8e550b307499713dffbfefb98a4932d4d7
Aneeq Khatri <aneeqabdulsamad5761@gmail.com>
2024-06-28 15:59:51 +0500

    cors error

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,32 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS
-  app.enableCors();
+  // Enable CORS
+  app.enableCors({
+    origin: 'https://pingup-frontend.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
+  // Handle preflight requests
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header(
+        'Access-Control-Allow-Origin',
+        'https://pingup-frontend.vercel.app',
+      );
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET,HEAD,PUT,PATCH,POST,DELETE',
+      );
+      res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Accept, Authorization',
+      );
+      return res.sendStatus(204);
+    }
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
